2defed997894f533ac6d3d8f894458f7dc7273cb
Chris Mena <chrisepm@MacBook-Air-de-Chris.local>
2025-06-27 01:14:11 -0600

    Fix asset paths for GitHub Pages deplyment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/FenixTeamLanding/',
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name]-[hash][extname]',
+              entryFileNames: 'assets/[name]-[hash].js,'
+        },
+      },
+    },
   server: {
     host: '0.0.0.0',
     port: 3000,
